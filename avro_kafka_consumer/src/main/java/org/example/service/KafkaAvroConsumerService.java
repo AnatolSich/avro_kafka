ee465ac40fcf04a54d910d907cd0465d323f01b3
Anatolii_Sichkar <tip-77@ukr.net>
2021-11-15 00:06:55 +0200

    Add message consumer

---
+++ avro_kafka_consumer/src/main/java/org/example/service/KafkaAvroConsumerService.java
@@ -0,0 +1,17 @@
+package org.example.service;
+
+import lombok.extern.slf4j.Slf4j;
+import org.example.KafkaMessage;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Service;
+
+@Slf4j
+@Service
+public class KafkaAvroConsumerService {
+
+    @KafkaListener(topics = "${kafka.topic}", groupId = "${spring.kafka.consumer.group-id}")
+    void consume(KafkaMessage data) {
+        log.info(data.toString());
+    }
+
+}
